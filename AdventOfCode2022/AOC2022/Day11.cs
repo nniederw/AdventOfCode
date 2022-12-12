1fe3c9ae9041b8e4d376243a4791a831d6198ce9
nniederw <niels.niederwieser@gmx.ch>
2022-12-13 00:31:46 +0100

    day11

--- AdventOfCode2022/AOC2022/Day11.cs
+++ AdventOfCode2022/AOC2022/Day11.cs
@@ -4,21 +4,180 @@
     {
         public static void Start()
         {
+            ReadMonkeysIn();
+            Console.WriteLine(Part1());
+            Monkey.Monkeys = new();
+            ReadMonkeysIn();
+            Console.WriteLine(Part2());
+        }
+        private static void ReadMonkeysIn()
+        {
             var lines = File.ReadLines($"{Global.InputPath}/Day11.txt");
+            List<Monkey> monkeys = new();
             int input = new();
             foreach (var line in lines)
             {
+                if (line == "") continue;
+                var l = line.Split(' ').Where(i => i != "").ToArray();
+                switch (l[0])
+                {
+                    case "Monkey":
+                        monkeys.Add(new Monkey());
+                        break;
+                    case "Starting":
+                        {
+                            var monk = monkeys.Last();
+                            foreach (var item in l.Skip(2))
+                            {
+                                monk.StartItems.Add(Convert.ToInt32(item.TrimEnd(',')));
+                            }
+                        }
+                        break;
+                    case "Operation:":
+                        monkeys.Last().Operation = new Operation(l[3], l[4][0], l[5]).Apply;
+                        break;
+                    case "Test:":
+                        int value = Convert.ToInt32(l[3]);
+                        monkeys.Last().Test = (long i) => i % value == 0;
+                        monkeys.Last().TestValue = value;
+                        break;
+
+                    case "If":
+                        if (l[1] == "true:")
+                        {
+                            monkeys.Last().IfTrue = (long item) => Monkey.ThrowTo(item, Convert.ToInt32(l[5]));
+                        }
+                        else
+                        {
+                            monkeys.Last().IfFalse = (long item) => Monkey.ThrowTo(item, Convert.ToInt32(l[5]));
+                        }
+                        break;
+                }
             }
-            Console.WriteLine(Part1(input));
-            Console.WriteLine(Part2(input));
+            for (int i = 0; i < monkeys.Count; i++)
+            {
+                if (Monkey.Monkeys[i] != monkeys[i]) throw new Exception();
+            }
         }
-        private static int Part1(int input)
+        private static long Part1()
         {
-            return -1;
+            var monkeys = Monkey.Monkeys;
+            for (int i = 0; i < 20; i++)
+            {
+                foreach (var monk in monkeys)
+                {
+                    monk.Act(i => i / 3);
+                }
+            }
+            monkeys.Sort((a, b) => b.ItemsInspected.CompareTo(a.ItemsInspected));
+            return monkeys[0].ItemsInspected * monkeys[1].ItemsInspected;
         }
-        private static int Part2(int input)
+        private static long Part2()
         {
-            return -1;
+            var monkeys = Monkey.Monkeys;
+            int modulo = 1;
+            foreach (var monk in monkeys)
+            {
+                modulo *= monk.TestValue;
+            }
+            for (int i = 0; i < 10000; i++)
+            {
+                foreach (var monk in monkeys)
+                {
+                    monk.Act(i => i % modulo);
+                }
+            }
+            monkeys.Sort((a, b) => b.ItemsInspected.CompareTo(a.ItemsInspected));
+            return monkeys[0].ItemsInspected * monkeys[1].ItemsInspected;
+        }
+    }
+    public class Monkey
+    {
+        public List<long> StartItems = new();
+        public Func<long, long>? Operation = null;
+        public Predicate<long>? Test = null;
+        public int TestValue = 0;
+        public Action<long>? IfTrue = null;
+        public Action<long>? IfFalse = null;
+        public static List<Monkey> Monkeys = new();
+        public long ItemsInspected = 0;
+        public Monkey()
+        {
+            Monkeys.Add(this);
+        }
+        private void ThrowItem(long item)
+        {
+            if (Test(item))
+            {
+                IfTrue(item);
+            }
+            else
+            {
+                IfFalse(item);
+            }
+        }
+        public static void ThrowTo(long item, int index)
+        {
+            Monkeys[index].StartItems.Add(item);
+        }
+        public void Act(Func<long, long> levelChange)
+        {
+            while (StartItems.Any())
+            {
+                long item = StartItems[0];
+                StartItems.RemoveAt(0);
+                item = Operation(item);
+                item = levelChange(item);
+                ThrowItem(item);
+                ItemsInspected++;
+            }
+        }
+    }
+    public class Operation
+    {
+        long value = int.MinValue;
+        private enum Identifier { value, old }
+        private Func<long, long, long> Op = (long a, long b) => int.MinValue;
+        Identifier Left = Identifier.value;
+        Identifier Right = Identifier.old;
+        public Operation(string identL, char op, string identR)
+        {
+            switch (op)
+            {
+                case '+':
+                    Op = (long a, long b) => { checked { return a + b; } };
+                    break;
+                case '*':
+                    Op = (long a, long b) => { checked { return a * b; } };
+                    break;
+                default: throw new NotImplementedException();
+            }
+            if (identL == "old")
+            {
+                Left = Identifier.old;
+            }
+            else
+            {
+                Left = Identifier.value;
+                value = Convert.ToInt32(identL);
+            }
+
+            if (identR == "old")
+            {
+                Right = Identifier.old;
+            }
+            else
+            {
+                Right = Identifier.value;
+                if (value != int.MinValue) throw new Exception();
+                value = Convert.ToInt32(identR);
+            }
+        }
+        public long Apply(long item)
+        {
+            long left = Left == Identifier.value ? value : item;
+            long right = Right == Identifier.value ? value : item;
+            return Op(left, right);
         }
     }
 }
--- AdventOfCode2022/AOC2022/Day12.cs
+++ AdventOfCode2022/AOC2022/Day12.cs
@@ -5,20 +5,141 @@
         public static void Start()
         {
             var lines = File.ReadLines($"{Global.InputPath}/Day12.txt");
-            int input = new();
+            int[,] input = null;
             foreach (var line in lines)
             {
+                if (input == null)
+                {
+                    input = new int[lines.Count(), line.Length];
+                }
             }
-            Console.WriteLine(Part1(input));
-            Console.WriteLine(Part2(input));
+            var S = FindS(input);
+            var E = FindE(input);
+            Console.WriteLine(Part1(input, S, E));
+            Console.WriteLine(Part2(input, S, E));
         }
-        private static int Part1(int input)
+        private static (int, int) FindS(int[,] input)
+        {
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    if (input[i, j] == 'S')
+                    {
+                        input[i, j] = 'a';
+                        return (i, j);
+                    }
+                }
+            }
+            throw new Exception();
+        }
+        private static (int, int) FindE(int[,] input)
+        {
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    if (input[i, j] == 'E')
+                    {
+                        input[i, j] = 'z';
+                        return (i, j);
+                    }
+                }
+            }
+            throw new Exception();
+        }
+        private static int Part1(int[,] input, (int, int) S, (int, int) E)
+        {
+            var G = new Graph(input.GetLength(0) * input.GetLength(1));
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = 0; j < input.GetLength(j); j++)
+                {
+                    int height = input[i, j];
+                    try
+                    {
+                        if (height - 1 <= input[i - 1, j] || input[i - 1, j] <= height + 1)
+                        {
+                            G.AddEdge(GetIndex(i, j), GetIndex(i - 1, j));
+                        }
+                    }
+                    catch (Exception e) { }
+                    try
+                    {
+                        if (height - 1 <= input[i + 1, j] || input[i + 1, j] <= height + 1)
+                        {
+                            G.AddEdge(GetIndex(i, j), GetIndex(i + 1, j));
+                        }
+                    }
+                    catch (Exception e) { }
+                    try
+                    {
+                        if (height - 1 <= input[i, j - 1] || input[i, j - 1] <= height + 1)
+                        {
+                            G.AddEdge(GetIndex(i, j), GetIndex(i, j - 1));
+                        }
+                    }
+                    catch (Exception e) { }
+                    try
+                    {
+                        if (height - 1 <= input[i, j + 1] || input[i, j + 1] <= height + 1)
+                        {
+                            G.AddEdge(GetIndex(i, j), GetIndex(i, j + 1));
+                        }
+                    }
+                    catch (Exception e) { }
+                }
+                int GetIndex(int i, int j)
+                {
+                    return i * input.GetLength(1) + j;
+                }
+
+            }
+
+
+            return -1;
+        }
+        private static int Part2(int[,] input, (int, int) S, (int, int) E)
         {
             return -1;
         }
-        private static int Part2(int input)
+    }
+    class Graph
+    {
+        public int n;
+        public int m;
+        public List<int>[] Edges;// edges from i = Edges[i]
+        public int[] Distance;
+        public Graph(int n)
         {
-            return -1;
+            Edges = new List<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                Edges[i] = new();
+            }
+        }
+        public void AddEdge(int v, int u)
+        {
+            Edges[v].Add(u);
+        }
+        public void BFS(int v)
+        {
+            Distance = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Distance[i] = int.MaxValue;
+            }
+            Distance[v] = 0;
+            HashSet<int> S = new HashSet<int>();
+            foreach (var edge in Edges[v])
+            {
+                S.Add(edge);
+            }
+            while (S.Any())
+            {
+                var newS = new HashSet<int>();
+                //todo
+            }
         }
     }
 }
