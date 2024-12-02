258cd2d4625aed4e5fc66646c2e78a0d5838bab2
nniederw <niels.niederwieser@gmx.ch>
2024-12-02 11:31:55 +0100

    u

--- AdventOfCode2024/AOC2024/Day01.cs
+++ AdventOfCode2024/AOC2024/Day01.cs
@@ -6,21 +6,41 @@
         public void Start()
         {
             var lines = File.ReadLines($"{Global.InputPath}/{TextFileName}");
-            List<string> input = new();
+            List<int> list1 = new();
+            List<int> list2 = new();
             foreach (var line in lines)
             {
-                input.Add(line);
+                var values = line.Split("   ");
+                list1.Add(Convert.ToInt32(values[0]));
+                list2.Add(Convert.ToInt32(values[1]));
             }
-            Console.WriteLine(Part1(input));
-            Console.WriteLine(Part2(input));
+            Console.WriteLine(Part1(list1.ToList(), list2.ToList())) ;
+            Console.WriteLine(Part2(list1.ToList(), list2.ToList()));
         }
-        private int Part1(List<string> input)
+        private int Part1(List<int> list1, List<int> list2)
         {
-            return -1;
+            list1.Sort();
+            list2.Sort();
+            int distance = 0;
+            for (int i = 0; i < list1.Count; i++)
+            {
+                distance += Distance(list1[i], list2[i]);
+            }
+            return distance;
         }
-        private int Part2(List<string> input)
+        private int Distance(int x, int y)
         {
-            return -1;
+            return Math.Abs(Math.Abs(x) - Math.Abs(y));
+        }
+        private int Part2(List<int> list1, List<int> list2)
+        {
+            int similarityScore = 0;
+            list1.Sort(); list2.Sort();
+            foreach (var location in list1)
+            {
+                similarityScore += location * list2.Where(i => i == location).Count();
+            }
+            return similarityScore;
         }
     }
 }
--- AdventOfCode2024/AOC2024/Day02.cs
+++ AdventOfCode2024/AOC2024/Day02.cs
@@ -6,21 +6,73 @@
         public void Start()
         {
             var lines = File.ReadLines($"{Global.InputPath}/{TextFileName}");
-            List<string> input = new();
+            List<List<int>> lists = new();
             foreach (var line in lines)
             {
-                input.Add(line);
+                var ints = line.Split(' ');
+                List<int> list = new();
+                foreach (var i in ints)
+                {
+                    list.Add(Convert.ToInt32(i));
+                }
+                lists.Add(list);
             }
-            Console.WriteLine(Part1(input));
-            Console.WriteLine(Part2(input));
+            Console.WriteLine(Part1(lists));
+            Console.WriteLine(Part2(lists));
         }
-        private int Part1(List<string> input)
+        private int Part1(List<List<int>> lists)
         {
-            return -1;
+            return lists.Count(Safe);
         }
-        private int Part2(List<string> calo)
+        private bool Safe(List<int> list)
         {
-            return -1;
+            return IncreasingSafe(list) || DecreasingSafe(list);
+        }
+        private bool IncreasingSafe(List<int> list)
+        {
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i - 1] >= list[i])
+                {
+                    return false;
+                }
+                if (list[i] - list[i - 1] > 3)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private bool DecreasingSafe(List<int> list)
+        {
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i - 1] <= list[i])
+                {
+                    return false;
+                }
+                if (list[i - 1] - list[i] > 3)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private bool ProblemDampenerSafe(List<int> list)
+        {
+            List<List<int>> lists = new();
+            lists.Add(list);
+            for (int i = 0; i < list.Count; i++)
+            {
+                var newlist = list.ToList();
+                newlist.RemoveAt(i);
+                lists.Add(newlist);
+            }
+            return !lists.TrueForAll(i=>!Safe(i));
+        }
+        private int Part2(List<List<int>> lists)
+        {
+            return lists.Count(ProblemDampenerSafe);
         }
     }
 }
