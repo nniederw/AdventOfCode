5fcbd2a494793a046dd3cf658bccb4b25e04f14a
nniederw <niels.niederwieser@gmx.ch>
2024-12-05 14:00:53 +0100

    u

--- AdventOfCode2024/AOC2024/Day04.cs
+++ AdventOfCode2024/AOC2024/Day04.cs
@@ -3,7 +3,6 @@
     class Day04 : IDay
     {
         private string TextFileName => $"{GetType().Name}.txt";
-        private const string ToSearch = "XMAS";
         public void Start()
         {
             var lines = File.ReadLines($"{Global.InputPath}/{TextFileName}");
@@ -31,29 +30,43 @@
         }
         private int Part1(char[,] board, int width, int height)
         {
-            string toSearchReverse = new string(ToSearch.Reverse().ToArray());
+            string SearchStr = "XMAS";
+            List<string> SearchSpace = new List<string> { SearchStr, new string(SearchStr.Reverse().ToArray()) };
             int result = 0;
-            int length = ToSearch.Length;
+            int length = SearchStr.Length;
             for (int x = 0; x < width; x++)
             {
                 for (int y = 0; y < height; y++)
                 {
-                    List<string> strs = new();
-                    try { strs.Add(GetString(board, x, y, 1, 0, length)); } catch { }
-                    try { strs.Add(GetString(board, x, y, 1, 1, length)); } catch { }
-                    try { strs.Add(GetString(board, x, y, 0, 1, length)); } catch { }
-                    try { strs.Add(GetString(board, x, y, -1, 1, length)); } catch { }
-                    result += strs.Count(i => i == ToSearch || i == toSearchReverse);
+                    result += new[]{
+                    GetString(board, x, y, 1, 0, length, width, height),
+                    GetString(board, x, y, 1, 1, length, width, height),
+                    GetString(board, x, y, 0, 1, length, width, height),
+                    GetString(board, x, y, -1, 1, length, width, height)
+                    }.Count(i => SearchSpace.Contains(i));
                 }
-                Console.WriteLine($"Searched column {x}, found {result} many XMAS");
             }
             return result;
         }
-        private bool InBound()
+        private bool InBound(int x, int y, int width, int height)
+            => 0 <= x && x < width && 0 <= y && y < height;
+        private IEnumerable<(int x, int y)> BoundedChain(int x, int y, int dx, int dy, int length, int width, int height)
+            => Chain(x, y, dx, dy, length).Where(i => InBound(i.x, i.y, width, height));
+        private IEnumerable<(int x, int y)> Chain(int x, int y, int dx, int dy, int length)
         {
-            return false;
+            for (int i = 0; i < length; i++)
+            {
+                yield return (x, y);
+                x += dx;
+                y += dy;
+            }
         }
-        private string GetString(char[,] board, int x, int y, int dx, int dy, int length)
+        private string GetString(char[,] board, int x, int y, int dx, int dy, int length, int width, int height)
+        {
+            var chain = BoundedChain(x, y, dx, dy, length, width, height);
+            return new string(chain.Select(i => board[i.x, i.y]).ToArray());
+        }
+        private string GetStringOld(char[,] board, int x, int y, int dx, int dy, int length)
         {
             string result = "";
             for (int i = 0; i < length; i++)
@@ -66,21 +79,24 @@
         }
         private int Part2(char[,] board, int width, int height)
         {
-            string toSearch = "MAS";
-            string toSearchReverse = new string(toSearch.Reverse().ToArray());
+            string SearchStr = "MAS";
+            List<string> SearchSpace = new List<string> { SearchStr, new string(SearchStr.Reverse().ToArray()) };
+
             int result = 0;
-            int length = toSearch.Length;
+            int length = SearchStr.Length;
             for (int x = 0; x < width; x++)
             {
                 for (int y = 0; y < height; y++)
                 {
-                    List<string> strs = new();
-                    try { strs.Add(GetString(board, x, y, 1, -1, length)); } catch { }
-                    try { strs.Add(GetString(board, x + 2, y, -1, -1, length)); } catch { }
-                    if (strs.Count(i => i == toSearch || i == toSearchReverse) == 2)
-                    { result++; }
+                    var matches = new[]{
+                    GetString(board, x, y, 1, -1, length, width, height),
+                    GetString(board, x + 2, y, -1, -1, length, width, height)
+                    }.Count(SearchSpace.Contains);
+                    if (matches == 2)
+                    {
+                        result++;
+                    }
                 }
-                Console.WriteLine($"Searched column {x}, found {result} many XMAS");
             }
             return result;
         }
