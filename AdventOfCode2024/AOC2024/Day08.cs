15b8a7a36474a92314b6172f9779eea17a81bd57
nniederw <niels.niederwieser@gmx.ch>
2024-12-09 09:57:46 +0100

    u

--- AdventOfCode2024/AOC2024/Day08.cs
+++ AdventOfCode2024/AOC2024/Day08.cs
@@ -11,16 +11,157 @@
             {
                 input.Add(line);
             }
-            Console.WriteLine(Part1(input));
-            Console.WriteLine(Part2(input));
+            if (!input.TrueForAll(i => i.Length == input.First().Length))
+            {
+                Console.WriteLine($"Something doesn't add up in {nameof(Day04)}");
+            }
+            int width = input.First().Length;
+            int height = input.Count;
+            char[,] map = new char[width, height];
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    map[x, y] = input[y][x];
+                }
+            }
+            Console.WriteLine(Part1(map, width, height));
+            Console.WriteLine(Part2(map, width, height));
         }
-        private int Part1(List<string> input)
+        private Dictionary<char,List<(int x,int y)>> GetAntennas(char[,] map, int width, int height)
         {
-            return -1;
+            char ignoreChar = '.';
+            Dictionary<char, List<(int x, int y)>> Antennas = new();
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    char c = map[x, y];
+                    if (c != ignoreChar)
+                    {
+                        if (!Antennas.ContainsKey(c))
+                        {
+                            Antennas[c] = new List<(int x, int y)>();
+                        }
+                        Antennas[c].Add((x, y));
+                    }
+                }
+            }
+            return Antennas;
         }
-        private int Part2(List<string> calo)
+        private int Part1(char[,] map, int width, int height)
         {
-            return -1;
+            char ignoreChar = '.';
+            Dictionary<char, List<(int x, int y)>> Antennas = GetAntennas(map, width, height);
+            int result = 0;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    bool hasAntinode = !Antennas.Select(i => i.Value).ToList().TrueForAll(i => !AntinodeOfAntenna(i, x, y));
+                    if (hasAntinode)
+                    {
+                        result++;
+                    }
+                }
+            }
+            return result;
+        }
+        private bool AntinodeOfAntenna(List<(int x, int y)> antennas, int x, int y)
+        {
+            foreach (var ant1 in antennas)
+            {
+                foreach (var ant2 in antennas)
+                {
+                    if (ant1 == ant2) continue;
+                    if (AntinodeOfAntennas(ant1, ant2, (x, y)))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private bool AntinodeOfAntennas((int x, int y) ant1, (int x, int y) ant2, (int x, int y) pos)
+        {
+            var dist1 = DirectionV1ToV2(ant1, pos);
+            var dist1x2 = (dist1.x * 2, dist1.y * 2);
+            var dist2 = DirectionV1ToV2(ant2, pos);
+            var dist2x2 = (dist2.x * 2, dist2.y * 2);
+            return (dist1 == dist2x2 || dist2 == dist1x2);
+        }
+        /*
+        private bool InLine((int x, int y) v1, (int x, int y) v2, (int x, int y) v3)
+        {
+            (int x, int y) dif12 = (v2.x - v1.x, v2.y - v1.y);
+            (int x, int y) dif23 = (v3.x - v2.x, v3.y - v2.y);
+            var sign12 = Math.Sign(dif12.x);
+            dif12 = (dif12.x * sign12, dif12.y * sign12);
+            var sign23 = Math.Sign(dif23.x);
+            dif23 = (dif23.x * sign23, dif23.y * sign23);
+            var ratio12x23x = new Rational(dif12.x, dif23.x);
+            var ratio12y23y = new Rational(dif12.y, dif23.y);
+            return ratio12x23x == ratio12y23y;
+        }*/
+        private (int x, int y) DirectionV1ToV2((int x, int y) v1, (int x, int y) v2)
+            => (v2.x - v1.x, v2.y - v1.y);
+        private bool TAlignedAntenna((int x, int y) ant1, (int x, int y) ant2, (int x, int y) pos)
+        {
+            if (pos == ant1 || pos == ant2) return true;
+            var dist12 = DirectionV1ToV2(ant1, ant2);
+            var dist2Pos = DirectionV1ToV2(ant2, pos);
+            int gcd12 = Gcd(dist12.x, dist12.y);
+            (int x, int y) direcAnt = (dist12.x / gcd12, dist12.y / gcd12);
+            if (dist2Pos.x % direcAnt.x != 0 || dist2Pos.y % direcAnt.y != 0) return false;
+            int multx = dist2Pos.x / direcAnt.x;
+            return direcAnt.y * multx == dist2Pos.y;
+        }
+        private bool TAlignedAntenna(List<(int x, int y)> antennas, int x, int y)
+        {
+            foreach (var ant1 in antennas)
+            {
+                foreach (var ant2 in antennas)
+                {
+                    if (ant1 == ant2) continue;
+                    if (TAlignedAntenna(ant1, ant2, (x, y)))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private static int Gcd(int a, int b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            int min = Math.Min(a, b);
+            int max = Math.Max(a, b);
+            if (min == 0) return max;
+            if (min == 1)
+            {
+                return 1;
+            }
+            max = Math.Max(a, b);
+            int mod = max % min;
+            return mod == 0 ? min : Gcd(min, mod);
+        }
+        private int Part2(char[,] map, int width, int height)
+        {
+            var Antennas = GetAntennas(map, width, height);
+            int result = 0;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    bool hasTAligned = !Antennas.Select(i => i.Value).ToList().TrueForAll(i => !TAlignedAntenna(i, x, y));
+                    if (hasTAligned)
+                    {
+                        result++;
+                    }
+                }
+            }
+            return result;
         }
     }
 }
--- AdventOfCode2024/AOC2024/Day09.cs
+++ AdventOfCode2024/AOC2024/Day09.cs
@@ -1,4 +1,6 @@
-﻿namespace AOC2024
+﻿using System.Runtime;
+
+namespace AOC2024
 {
     class Day09 : IDay
     {
@@ -6,21 +8,143 @@
         public void Start()
         {
             var lines = File.ReadLines($"{Global.InputPath}/{TextFileName}");
-            List<string> input = new();
+            List<int> input = new();
             foreach (var line in lines)
             {
-                input.Add(line);
+                foreach (var c in line)
+                {
+                    input.Add(Convert.ToInt32(c.ToString()));
+                }
             }
             Console.WriteLine(Part1(input));
             Console.WriteLine(Part2(input));
         }
-        private int Part1(List<string> input)
+        private List<int> Expand(List<int> compressedFiles)
         {
-            return -1;
+            List<int> result = new();
+            bool file = true;
+            int id = 0;
+            for (int i = 0; i < compressedFiles.Count; i++)
+            {
+                int compNumber = compressedFiles[i];
+                if (file)
+                {
+                    Enumerable.Range(0, compNumber).ToList().ForEach(j => result.Add(id));
+                    id++;
+                }
+                else
+                {
+                    Enumerable.Range(0, compNumber).ToList().ForEach(j => result.Add(-1));
+                }
+                file = !file;
+            }
+            return result;
         }
-        private int Part2(List<string> calo)
+        private long Checksum(List<int> files)
         {
-            return -1;
+            long sum = 0;
+            for (int i = 0; i < files.Count; i++)
+            {
+                long file = files[i];
+                if (file == -1) { continue; }
+                sum += file * i;
+            }
+            return sum;
+        }
+        private bool RearangeOnce(List<int> files)
+        {
+            int indexLastUsedBlock = -1;
+            for (int i = files.Count - 1; i > 0; i--)
+            {
+                if (files[i] != -1)
+                {
+                    indexLastUsedBlock = i;
+                    break;
+                }
+            }
+            if (indexLastUsedBlock == -1) { return false; }
+            int indexFirstFreeBlock = -1;
+            for (int i = 0; i < files.Count; i++)
+            {
+                if (files[i] == -1)
+                {
+                    indexFirstFreeBlock = i;
+                    break;
+                }
+            }
+            if (indexFirstFreeBlock == -1) { return false; }
+            if (indexFirstFreeBlock > indexLastUsedBlock) { return false; }
+            files[indexFirstFreeBlock] = files[indexLastUsedBlock];
+            files[indexLastUsedBlock] = -1;
+            return true;
+        }
+        private long Part1(List<int> files)
+        {
+            files = Expand(files);
+            while (RearangeOnce(files)) { }
+            return Checksum(files);
+        }
+        private List<(int Id, int StartIndex, int Length)> GetAllBlocks(List<int> files)
+        {
+            List<(int Id, int StartIndex, int Length)> result = new();
+            int currentBlock = files.First();
+            int currentLength = -1;
+            int startIndex = 0;
+            for (int i = 0; i < files.Count; i++)
+            {
+                if (files[i] == currentBlock)
+                {
+                    currentLength++;
+                }
+                else
+                {
+                    result.Add((currentBlock, startIndex, currentLength + 1));
+                    currentBlock = files[i];
+                    currentLength = 0;
+                    startIndex = i;
+                }
+            }
+            if (currentLength > 0)
+            {
+                result.Add((currentBlock, startIndex, currentLength + 1));
+            }
+            return result;
+        }
+        private List<(int StartIndex, int Length)> GetUsedBlocks(List<int> files)
+            => GetAllBlocks(files).Where(i => i.Id != -1).Select(i => (i.StartIndex, i.Length)).ToList();
+        private List<(int StartIndex, int Length)> GetFreeBlocks(List<int> files)
+            => GetAllBlocks(files).Where(i => i.Id == -1).Select(i => (i.StartIndex, i.Length)).ToList();
+        private bool TryRearangeBlockOnce(List<int> files, int BlockStartIndex, int BlockLength)
+        {
+            var freeBlocks = GetFreeBlocks(files);
+            foreach (var i in freeBlocks)
+            {
+                if (i.Length >= BlockLength &&i.StartIndex < BlockStartIndex)
+                {
+                    RearangeBlock(files, BlockStartIndex, BlockLength, i.StartIndex);
+                    return true;
+                }
+            }
+            return false;
+        }
+        private void RearangeBlock(List<int> files, int BlockStartIndex, int BlockLength, int newIndex)
+        {
+            for (int i = 0; i < BlockLength; i++)
+            {
+                files[newIndex + i] = files[BlockStartIndex + i];
+                files[BlockStartIndex + i] = -1;
+            }
+        }
+        private long Part2(List<int> files)
+        {
+            files = Expand(files);
+            var blocks = GetUsedBlocks(files);
+            blocks.Reverse();
+            foreach (var block in blocks)
+            {
+                TryRearangeBlockOnce(files, block.StartIndex, block.Length);
+            }
+            return Checksum(files);
         }
     }
 }
