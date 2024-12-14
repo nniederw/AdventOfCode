e162e4be62e0efed71428984c02f9dc5fd17c933
nniederw <niels.niederwieser@gmx.ch>
2024-12-14 01:30:12 +0100

    u

--- AdventOfCode2024/AOC2024/Day13.cs
+++ AdventOfCode2024/AOC2024/Day13.cs
@@ -9,18 +9,92 @@
             List<string> input = new();
             foreach (var line in lines)
             {
-                input.Add(line);
+                if (line != "")
+                {
+                    input.Add(line);
+                }
             }
-            Console.WriteLine(Part1(input));
-            Console.WriteLine(Part2(input));
+            List<SlotMachine> slotMachines = new();
+            if (input.Count % 3 != 0) { throw new Exception($"Input of {nameof(Day13)} was in a wrong format"); }
+            for (int i = 0; i < input.Count; i += 3)
+            {
+                var s1 = input[i].Substring(12).Split(", Y+");
+                var s2 = input[i + 1].Substring(12).Split(", Y+");
+                var s3 = input[i + 2].Substring(9).Split(", Y=");
+                (int x, int y) a = (Convert.ToInt32(s1[0]), Convert.ToInt32(s1[1]));
+                (int x, int y) b = (Convert.ToInt32(s2[0]), Convert.ToInt32(s2[1]));
+                (int x, int y) prize = (Convert.ToInt32(s3[0]), Convert.ToInt32(s3[1]));
+                slotMachines.Add(new SlotMachine(a, b, prize));
+            }
+            Console.WriteLine(Part1(slotMachines));
+            Console.WriteLine(Part2(slotMachines));
         }
-        private int Part1(List<string> input)
+        private bool MutliSolution(SlotMachine machine)
         {
-            return -1;
+            var A = machine.A;
+            var B = machine.B;
+            var P = machine.Prize;
+            var RatA = new LongRational(A.x, A.y);
+            var RatB = new LongRational(B.x, B.y);
+            var RatP = new LongRational(P.x, P.y);
+            return (RatA == RatB) && (RatB == RatP);
         }
-        private int Part2(List<string> calo)
+        private bool HasZeroInIt(SlotMachine machine)
         {
-            return -1;
+            return machine.A.x == 0 || machine.A.y == 0 || machine.B.x == 0 || machine.B.y == 0 || machine.Prize.x == 0 || machine.Prize.y == 0;
+        }
+        private long? TokensNeeded(SlotMachine machine, bool CheckUnder100 = true)
+        {
+            if (HasZeroInIt(machine)) { return null; } //these cases aren't handled currently
+            var A = machine.A;
+            var B = machine.B;
+            var P = machine.Prize;
+            long ax = A.x;
+            long ay = A.y;
+            long bx = B.x;
+            long by = B.y;
+            long px = P.x;
+            long py = P.y;
+            //try to make k*A + m*B = P
+            long mtop = py * ax - ay * px;
+            long mbot = by * ax - ay * bx;
+            if (mbot == 0) { return null; }
+            var RatM = new LongRational(mtop, mbot);
+            if (!RatM.IsInteger()) { return null; }
+            long m = RatM.Numerator;
+            var RatK = new LongRational(px - m * bx, ax);
+            if (!RatK.IsInteger()) { return null; }
+            long k = RatK.Numerator;
+            if (CheckUnder100 && (k > 100 || m > 100)) { return null; }
+            if (k < 0 || m < 0) { return null; }
+            return k * 3 + m;
+        }
+        private long Part1(IReadOnlyList<SlotMachine> SlotMachines)
+        {
+            if (SlotMachines.Where(MutliSolution).Any()) throw new Exception($"Multisolutions aren't currently handled in Day13");
+            if (SlotMachines.Where(HasZeroInIt).Any()) throw new Exception($"Slotmachines can't contain zeros currently handled in Day13");
+            return SlotMachines.Select(i => TokensNeeded(i)).Where(i => (i != null)).Select(i => i.Value).Sum();
+        }
+
+        private long Part2(IReadOnlyList<SlotMachine> SlotMachines)
+        {
+            long error = 10000000000000;
+            var CorrectedMachines = SlotMachines.Select(i => new SlotMachine(i.A, i.B, (i.Prize.x + error, i.Prize.y + error))).ToList();
+            if (CorrectedMachines.Where(MutliSolution).Any()) throw new Exception($"Multisolutions aren't currently handled in Day13");
+            if (CorrectedMachines.Where(HasZeroInIt).Any()) throw new Exception($"Slotmachines can't contain zeros currently handled in Day13");
+            return CorrectedMachines.Select(i => TokensNeeded(i, false)).Where(i => (i != null)).Select(i => i.Value).Sum(); ;
+        }
+        private struct SlotMachine
+        {
+            public (long x, long y) A;
+            public (long x, long y) B;
+            public (long x, long y) Prize;
+            public SlotMachine((long x, long y) a, (long x, long y) b, (long x, long y) prize)
+            {
+                A = a;
+                B = b;
+                Prize = prize;
+            }
         }
     }
 }
--- AdventOfCode2024/AOC2024/Rational.cs
+++ AdventOfCode2024/AOC2024/Rational.cs
@@ -1,37 +1,39 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
 
 public struct Rational
 {
     public int Denominator { get; private set; }
-    public int Nominator { get; private set; }
+    public int Numerator { get; private set; }
     public Rational()
     {
-        Nominator = 0;
+        Numerator = 0;
         Denominator = 1;
     }
     public Rational(int value)
     {
-        Nominator = value;
+        Numerator = value;
         Denominator = 1;
     }
-    public Rational(int nomi, int denom)
+    public Rational(int nume, int denom)
     {
         Denominator = denom;
-        Nominator = nomi;
+        Numerator = nume;
         Normalize();
     }
     public static Rational Zero => new Rational(0);
     public static Rational One => new Rational(1);
     private void Normalize()
     {
-        if (Nominator == 0)
+        if (Numerator == 0)
         {
             Denominator = 1;
             return;
         }
-        int gcd = Gcd(Nominator, Denominator);
-        Denominator /= gcd;
-        Nominator /= gcd;
+        int gcd = Gcd(Numerator, Denominator);
+        int sign = Math.Sign(Denominator);
+        Denominator /= gcd * sign;
+        Numerator /= gcd * sign;
     }
     private static int Gcd(int a, int b)
     {
@@ -52,24 +54,25 @@ public struct Rational
     {
         return checked(a * (b / Gcd(a, b)));
     }
-    public decimal ToDecimal() => ((decimal)Nominator) / Denominator;
-    public double ToDouble() => ((double)Nominator) / Denominator;
+    public decimal ToDecimal() => ((decimal)Numerator) / Denominator;
+    public double ToDouble() => ((double)Numerator) / Denominator;
+    public bool IsInteger() => Denominator == 1;
     public static Rational operator +(Rational a, Rational b)
     {
         int lcm = Lcm(a.Denominator, b.Denominator);
         int amult = lcm / a.Denominator;
         int bmult = lcm / b.Denominator;
-        int nom = a.Nominator * amult + b.Nominator * bmult;
+        int nom = a.Numerator * amult + b.Numerator * bmult;
         return new Rational(nom, lcm);
     }
     public static Rational operator -(Rational a, Rational b)
     => a + (new Rational(-1) * b);
     public static Rational operator *(Rational a, Rational b)
-            => new Rational(a.Nominator * b.Nominator, a.Denominator * b.Denominator);
+            => new Rational(a.Numerator * b.Numerator, a.Denominator * b.Denominator);
     public static Rational operator /(Rational a, Rational b)
-            => a * new Rational(b.Denominator, b.Nominator);
+            => a * new Rational(b.Denominator, b.Numerator);
     public static bool operator ==(Rational a, Rational b)
-        => a.Nominator == b.Nominator && a.Denominator == b.Denominator;
+        => a.Numerator == b.Numerator && a.Denominator == b.Denominator;
     public static bool operator !=(Rational a, Rational b)
         => !(a == b);
     public static bool operator <(Rational a, Rational b)
@@ -77,14 +80,14 @@ public struct Rational
         int lcm = Lcm(a.Denominator, b.Denominator);
         int amult = lcm / a.Denominator;
         int bmult = lcm / b.Denominator;
-        return a.Nominator * amult < b.Nominator * bmult;
+        return a.Numerator * amult < b.Numerator * bmult;
     }
     public static bool operator >(Rational a, Rational b)
     {
         int lcm = Lcm(a.Denominator, b.Denominator);
         int amult = lcm / a.Denominator;
         int bmult = lcm / b.Denominator;
-        return a.Nominator * amult > b.Nominator * bmult;
+        return a.Numerator * amult > b.Numerator * bmult;
     }
     public override bool Equals([NotNullWhen(true)] object obj)
     {
@@ -92,11 +95,200 @@ public struct Rational
         {
             return (Rational)obj == this;
         }
-        throw new Exception();
+        throw new Exception($"Called {nameof(Rational)}.{nameof(Equals)} with a non {nameof(Rational)} object");
     }
     public bool Equals(Rational other) => this == other;
-    public override int GetHashCode() => Nominator + Denominator;
+    public override int GetHashCode() => Numerator + Denominator;
     public override string ToString() => ToString(false);
     public string ToString(bool Float)
-        => Float ? (((double)Nominator) / Denominator).ToString() : $"{Nominator} / {Denominator}";
+        => Float ? (((double)Numerator) / Denominator).ToString() : $"{Numerator} / {Denominator}";
+}
+public struct LongRational
+{
+    public long Denominator { get; private set; }
+    public long Numerator { get; private set; }
+    public LongRational()
+    {
+        Numerator = 0;
+        Denominator = 1;
+    }
+    public LongRational(long value)
+    {
+        Numerator = value;
+        Denominator = 1;
+    }
+    public LongRational(long nume, long denom)
+    {
+        Denominator = denom;
+        Numerator = nume;
+        Normalize();
+    }
+    public static LongRational Zero => new LongRational(0);
+    public static LongRational One => new LongRational(1);
+    private void Normalize()
+    {
+        if (Numerator == 0)
+        {
+            Denominator = 1;
+            return;
+        }
+        long gcd = Gcd(Numerator, Denominator);
+        long sign = Math.Sign(Denominator);
+        Denominator /= gcd * sign;
+        Numerator /= gcd * sign;
+    }
+    private static long Gcd(long a, long b)
+    {
+        a = Math.Abs(a);
+        b = Math.Abs(b);
+        long min = Math.Min(a, b);
+        long max = Math.Max(a, b);
+        if (min == 0) return max;
+        if (min == 1)
+        {
+            return 1;
+        }
+        max = Math.Max(a, b);
+        long mod = max % min;
+        return mod == 0 ? min : Gcd(min, mod);
+    }
+    public static long Lcm(long a, long b)
+    {
+        return checked(a * (b / Gcd(a, b)));
+    }
+    public decimal ToDecimal() => ((decimal)Numerator) / Denominator;
+    public double ToDouble() => ((double)Numerator) / Denominator;
+    public bool IsInteger() => Denominator == 1;
+    public static LongRational operator +(LongRational a, LongRational b)
+    {
+        long lcm = Lcm(a.Denominator, b.Denominator);
+        long amult = lcm / a.Denominator;
+        long bmult = lcm / b.Denominator;
+        long nom = a.Numerator * amult + b.Numerator * bmult;
+        return new LongRational(nom, lcm);
+    }
+    public static LongRational operator -(LongRational a, LongRational b)
+    => a + (new LongRational(-1) * b);
+    public static LongRational operator *(LongRational a, LongRational b)
+            => new LongRational(a.Numerator * b.Numerator, a.Denominator * b.Denominator);
+    public static LongRational operator /(LongRational a, LongRational b)
+            => a * new LongRational(b.Denominator, b.Numerator);
+    public static bool operator ==(LongRational a, LongRational b)
+        => a.Numerator == b.Numerator && a.Denominator == b.Denominator;
+    public static bool operator !=(LongRational a, LongRational b)
+        => !(a == b);
+    public static bool operator <(LongRational a, LongRational b)
+    {
+        long lcm = Lcm(a.Denominator, b.Denominator);
+        long amult = lcm / a.Denominator;
+        long bmult = lcm / b.Denominator;
+        return a.Numerator * amult < b.Numerator * bmult;
+    }
+    public static bool operator >(LongRational a, LongRational b)
+    {
+        long lcm = Lcm(a.Denominator, b.Denominator);
+        long amult = lcm / a.Denominator;
+        long bmult = lcm / b.Denominator;
+        return a.Numerator * amult > b.Numerator * bmult;
+    }
+    public override bool Equals([NotNullWhen(true)] object obj)
+    {
+        if (obj is LongRational)
+        {
+            return (LongRational)obj == this;
+        }
+        throw new Exception($"Called {nameof(LongRational)}.{nameof(Equals)} with a non {nameof(LongRational)} object");
+    }
+    public bool Equals(LongRational other) => this == other;
+    public override int GetHashCode() => Numerator.GetHashCode() + Denominator.GetHashCode();
+    public override string ToString() => ToString(false);
+    public string ToString(bool Float)
+        => Float ? (((double)Numerator) / Denominator).ToString() : $"{Numerator} / {Denominator}";
+}
+public struct BigRational
+{
+    public BigInteger Denominator { get; private set; }
+    public BigInteger Numerator { get; private set; }
+    public BigRational()
+    {
+        Numerator = 0;
+        Denominator = 1;
+    }
+    public BigRational(BigInteger value)
+    {
+        Numerator = value;
+        Denominator = 1;
+    }
+    public BigRational(BigInteger nume, BigInteger denom)
+    {
+        Denominator = denom;
+        Numerator = nume;
+        Normalize();
+    }
+    public static BigRational Zero => new BigRational(0);
+    public static BigRational One => new BigRational(1);
+    private void Normalize()
+    {
+        if (Numerator == 0)
+        {
+            Denominator = 1;
+            return;
+        }
+        BigInteger gcd = BigInteger.GreatestCommonDivisor(Numerator, Denominator);
+        BigInteger sign = Denominator.Sign;
+        Denominator /= gcd * sign;
+        Numerator /= gcd * sign;
+    }
+    public static BigInteger Lcm(BigInteger a, BigInteger b)
+    {
+        return checked(a * (b / BigInteger.GreatestCommonDivisor(a, b)));
+    }
+    public decimal ToDecimal() => ((decimal)Numerator) / (decimal)Denominator;
+    public double ToDouble() => ((double)Numerator) / (double)Denominator;
+    public bool IsInteger() => Denominator == 1;
+    public static BigRational operator +(BigRational a, BigRational b)
+    {
+        BigInteger lcm = Lcm(a.Denominator, b.Denominator);
+        BigInteger amult = lcm / a.Denominator;
+        BigInteger bmult = lcm / b.Denominator;
+        BigInteger nom = a.Numerator * amult + b.Numerator * bmult;
+        return new BigRational(nom, lcm);
+    }
+    public static BigRational operator -(BigRational a, BigRational b)
+    => a + (new BigRational(-1) * b);
+    public static BigRational operator *(BigRational a, BigRational b)
+            => new BigRational(a.Numerator * b.Numerator, a.Denominator * b.Denominator);
+    public static BigRational operator /(BigRational a, BigRational b)
+            => a * new BigRational(b.Denominator, b.Numerator);
+    public static bool operator ==(BigRational a, BigRational b)
+        => a.Numerator == b.Numerator && a.Denominator == b.Denominator;
+    public static bool operator !=(BigRational a, BigRational b)
+        => !(a == b);
+    public static bool operator <(BigRational a, BigRational b)
+    {
+        BigInteger lcm = Lcm(a.Denominator, b.Denominator);
+        BigInteger amult = lcm / a.Denominator;
+        BigInteger bmult = lcm / b.Denominator;
+        return a.Numerator * amult < b.Numerator * bmult;
+    }
+    public static bool operator >(BigRational a, BigRational b)
+    {
+        BigInteger lcm = Lcm(a.Denominator, b.Denominator);
+        BigInteger amult = lcm / a.Denominator;
+        BigInteger bmult = lcm / b.Denominator;
+        return a.Numerator * amult > b.Numerator * bmult;
+    }
+    public override bool Equals([NotNullWhen(true)] object obj)
+    {
+        if (obj is BigRational)
+        {
+            return (BigRational)obj == this;
+        }
+        throw new Exception($"Called {nameof(BigRational)}.{nameof(Equals)} with a non {nameof(BigRational)} object");
+    }
+    public bool Equals(BigRational other) => this == other;
+    public override int GetHashCode() => Numerator.GetHashCode() + Denominator.GetHashCode();
+    public override string ToString() => ToString(false);
+    public string ToString(bool Float)
+        => Float ? (((double)Numerator) / (double)Denominator).ToString() : $"{Numerator} / {Denominator}";
 }
